9decf3c04733dba010f8e7b155030645f66dd058
Zabihamdam <45077054+Zabihamdam@users.noreply.github.com>
2019-01-19 13:03:33 -0500

    added "System.out.println("Good evening");" line

--- src/HelloWord.java
+++ src/HelloWord.java
@@ -2,5 +2,6 @@ public class HelloWord {
     public static void main(String[] args){
         System.out.println("hello Word");
         System.out.println("Good evening");
+        System.out.println("how are you");
     }
 }
